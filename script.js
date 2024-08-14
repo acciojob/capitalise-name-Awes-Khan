925cc303d4b71405c4e0c0eb3779c00b0e5d57d9
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-14 21:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function capitalize() {
+    const inputField = document.getElementById('fname');
+    inputField.value = inputField.value.toUpperCase();
+}
+
+document.getElementById('fname').addEventListener('blur', capitalize);
